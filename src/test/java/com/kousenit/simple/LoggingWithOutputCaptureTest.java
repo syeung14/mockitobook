ce6bd11e5bca256eb9e20a58ab1cdfc1fd1c75ba
Marco Yeung <mssyeung@amazon.com>
2024-01-13 11:57:08 -0500

    learnig

--- src/test/java/com/kousenit/simple/LoggingWithOutputCaptureTest.java
+++ src/test/java/com/kousenit/simple/LoggingWithOutputCaptureTest.java
@@ -3,25 +3,25 @@ package com.kousenit.simple;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.system.CapturedOutput;
-import org.springframework.boot.test.system.OutputCaptureExtension;
+//import org.springframework.boot.test.system.CapturedOutput;
+//import org.springframework.boot.test.system.OutputCaptureExtension;
 
 import java.util.logging.Logger;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Disabled("Working locally, but failing as a GitHub Action")
-@ExtendWith(OutputCaptureExtension.class)
+//@ExtendWith(OutputCaptureExtension.class)
 public class LoggingWithOutputCaptureTest {
-    @Test
-    void testLogWithOutputCapture(CapturedOutput outputCapture) {
-        Logger logger = Logger.getLogger(LoggingDemoTests.class.getName());
-        LoggingDemo demo = new LoggingDemo(logger);
-        demo.doStuff("Hello, world!");
-
-        assertThat(outputCapture.getOut())
-                .contains("Doing useful stuff: Hello, world!");
-        assertThat(outputCapture.getErr())
-                .contains("INFO: Hello, world!");
-    }
+//    @Test
+//    void testLogWithOutputCapture(CapturedOutput outputCapture) {
+//        Logger logger = Logger.getLogger(LoggingDemoTests.class.getName());
+//        LoggingDemo demo = new LoggingDemo(logger);
+//        demo.doStuff("Hello, world!");
+//
+//        assertThat(outputCapture.getOut())
+//                .contains("Doing useful stuff: Hello, world!");
+//        assertThat(outputCapture.getErr())
+//                .contains("INFO: Hello, world!");
+//    }
 }
